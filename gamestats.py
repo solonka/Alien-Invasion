f17a9d549874a6ece349e90e1038b71e14c60778
solonka <sinkeriel@gmail.com>
2021-06-09 10:34:14 +0300

    final commit

---
+++ gamestats.py
@@ -0,0 +1,13 @@
+import pygame
+
+class GameStats:
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.game_active = False
+        self.reset_stats()
+        self.high_score = 0
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
